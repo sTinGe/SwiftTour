91da5b23daf301089439ff1ffd4ce8e1619fb8ac
sTinGe <stinge62580@gmail.com>
2015-06-30 23:53:47 +0800

    first commit

---
+++ SwiftTour/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftTour
+//
+//  Created by sTinGe Su on 2015/6/30.
+//  Copyright (c) 2015年 sTinGe Su. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
